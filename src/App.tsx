2e2ec44879c4a2df215579bf86d89c5bce6a2de3
joaovrcosta <joaovr.costa@gmail.com>
2025-05-24 12:29:49 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 4px;
+  background: #fff;
+  color: #000;
+`;
